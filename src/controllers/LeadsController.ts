9169b079d07d7f76680d9117764f66c8caed4ed6
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2024-10-18 20:52:46 -0300

    Pagination and filtering of leads

--- src/controllers/LeadsController.ts
+++ src/controllers/LeadsController.ts
@@ -2,15 +2,41 @@ import { Handler } from "express";
 import { prisma } from "../database";
 import {
   CreateLeadRequestSchema,
+  GetLeadsRequestSchema,
   UpdateLeadRequestSchema,
 } from "../schemas/LeadsRequestSchema";
 import { HttpError } from "../errors/HttpError";
+import { Prisma } from "@prisma/client";
 
 export class LeadsController {
   index: Handler = async (req, res, next) => {
     try {
-      const leads = await prisma.lead.findMany();
-      res.json(leads);
+      const query = GetLeadsRequestSchema.parse(req.query);
+      const { page = "1", pageSize = "10", name, status, sortBy = "name", order = "asc" } = query
+
+      const where: Prisma.LeadWhereInput = {}
+
+      if (name) where.name = { contains: name, mode: "insensitive" }
+      if (status) where.status = status
+
+      const leads = await prisma.lead.findMany({
+        where,
+        // Ex: se o page for 2 e o page size for 10, vai tirar 1 do page e multiplicar por 10. Então pulando 10 resultados, e aparecendo a partir do 11
+        skip: (+page - 1) * +pageSize,
+        orderBy: { [ sortBy ]: order}
+      });
+
+      const total = await prisma.lead.count({ where })
+
+      res.json({
+        data: leads,
+        pagination: {
+            page: +page,
+            pageSize: +pageSize,
+            total,
+            totalPages: Math.ceil(total / +pageSize)
+            }
+      });
     } catch (error) {
       next(error);
     }
