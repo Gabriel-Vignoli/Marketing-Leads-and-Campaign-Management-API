79eba8bd5499d5314c99e25b98151ff5c26742aa
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2024-10-18 15:22:14 -0300

    Implementation of Lead Creation, Listing, Updating, and Deletion

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,17 @@
 import { Router } from "express";
 import { HttpError } from "./errors/HttpError";
+import { LeadsController } from "./controllers/LeadsController";
 
 const router = Router();
+
+const leadsController = new LeadsController()
+
+router.get("/leads", leadsController.index)
+router.post("/leads", leadsController.create)
+router.get("/leads/:id", leadsController.show)
+router.put("/leads/:id", leadsController.update)
+router.delete("/leads/:id", leadsController.delete)
+
 
 router.get("/status", (req, res, next) => {
   try {
