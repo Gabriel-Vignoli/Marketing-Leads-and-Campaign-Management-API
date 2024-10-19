115f6e870f29b105475f96a10d9804a490c61e86
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2024-10-19 07:59:38 -0300

    Creating routes for groups

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,25 @@
 import { Router } from "express";
 import { HttpError } from "./errors/HttpError";
 import { LeadsController } from "./controllers/LeadsController";
+import { GroupsController } from "./controllers/GroupsController";
 
 const router = Router();
 
 const leadsController = new LeadsController()
+const groupsController = new GroupsController()
 
 router.get("/leads", leadsController.index)
 router.post("/leads", leadsController.create)
 router.get("/leads/:id", leadsController.show)
 router.put("/leads/:id", leadsController.update)
 router.delete("/leads/:id", leadsController.delete)
+
+
+router.get("/groups", groupsController.index)
+router.post("/groups", groupsController.create)
+router.get("/groups/:id", groupsController.show)
+router.put("/groups/:id", groupsController.update)
+router.delete("/groups/:id", groupsController.delete)
 
 
 router.get("/status", (req, res, next) => {
@@ -21,5 +30,5 @@ router.get("/status", (req, res, next) => {
     next(error)
   }
 });
-
+  
 export { router };
